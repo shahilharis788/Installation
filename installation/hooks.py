18e270ef9d15a2d31f291b0727b522b10b0763d4
shahilharis788 <shahilharisaifer@gmail.com>
2025-09-18 12:17:43 +0530

    feat: added hooks

--- installation/hooks.py
+++ installation/hooks.py
@@ -43,7 +43,7 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {"Delivery Note" : "public/js/create_installation_request.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
@@ -138,10 +138,8 @@ app_license = "mit"
 # Hook on document methods and events
 
 # doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
+# 	"Sales Invoice": {
+# 		"validate": "installation.tasks.test"
 # 	}
 # }
 
